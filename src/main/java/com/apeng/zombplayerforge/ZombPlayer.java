9ade2e8d1db250485d01ddb8044fe00e36f772c6
A_Peng215 <976310604@qq.com>
2024-02-11 15:45:30 +0800

    Initial commit

---
+++ src/main/java/com/apeng/zombplayerforge/ZombPlayer.java
@@ -0,0 +1,12 @@
+package com.apeng.zombplayerforge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ZombPlayer.MOD_ID)
+public class ZombPlayer {
+
+    public static final String MOD_ID = "zombplayer";
+
+    public ZombPlayer() {}
+
+}
