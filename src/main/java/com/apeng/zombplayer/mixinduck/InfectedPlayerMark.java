a375892f178420b27269c7f9f783206e06b81c90
A_Peng215 <976310604@qq.com>
2024-02-16 11:34:46 +0800

    Add java doc

--- src/main/java/com/apeng/zombplayer/mixinduck/InfectedPlayerMark.java
+++ src/main/java/com/apeng/zombplayer/mixinduck/InfectedPlayerMark.java
@@ -2,8 +2,15 @@ package com.apeng.zombplayer.mixinduck;
 
 public interface InfectedPlayerMark {
 
+    /**
+     * Check if the entity is converted from player
+     * @return if the entity is converted from player
+     */
     boolean isInfectedPlayer();
 
+    /**
+     * Mark the entity as a infected player
+     */
     void setAsInfectedPlayer();
 
 }
